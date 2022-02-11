64bc63bc458f98a6e09f95842f7bdaa392314be8
tallglassofwalter <73105310+tallglassofwalter@users.noreply.github.com>
2022-02-10 18:56:41 -0800

    exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,121 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
-  }, [])
+  }, []);
+
+  const addBlog = (e) => {
+    e.preventDefault();
+    // const blogObj = {
+    //   title: ,
+    //   url,
+    //   likes,
+    //   user
+    // }
+    const blogObj = {};
+    blogService
+      .create(blogObj)
+      .then((returnedBlog) => {
+        setBlogs(blogs.concat(returnedBlog));
+        setNewBlog('');
+      });
+  }
+
+  const handleBlogChange = (e) => {
+    setNewBlog(e.target.value);
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({
+        username, password
+      });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  );
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <Notification message={errorMessage} />
+
+      {
+        user === null ?
+          loginForm() :
+          <div>
+            <p>{user.name} logged in</p>
+            {noteForm()}
+          </div>
+      }
+
+      <div>
+        <ul>
+          {
+            user !== null ? blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            ) : null
+          }
+        </ul>
+        <Footer />
+      </div>
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,19 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
+
+const create = (newObj) => {
+  const request = axios.post(baseUrl, newObj);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObj) => {
+  const request = axios.get(`${baseUrl}/${id}`, newObj);
+  return request.then((response) => response.data);
 }
 
-export default { getAll }
+export default { getAll, create, update };
